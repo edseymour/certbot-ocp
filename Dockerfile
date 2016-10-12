3bf1cf1d91465431cd94fc75dc4ba0b36cef5d25
edseymour <eseymour@redhat.com>
2016-10-12 12:44:49 +0100

    added OCP integration

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM rhel
 USER root
 ADD https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm /epel-release-latest-7.noarch.rpm
 RUN yum install -y /epel-release-latest-7.noarch.rpm
-RUN yum install --enablerepo=rhel-7-server-optional-rpms -y certbot && yum clean all
+RUN yum install --enablerepo=rhel-7-server-optional-rpms --enablerepo=rhel-7-server-ose-3.3-rpms -y certbot atomic-openshift-clients && yum clean all
 RUN mkdir -p /opt/letsencrypt/certs && chown 1001:1001 -R /opt/letsencrypt
 WORKDIR /opt/letsencrypt
 ADD get-certs.sh /opt/letsencrypt/get-certs.sh 
--- get-certs.sh
+++ get-certs.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-echo "*** Running certbot in standalone mode, expecting additional parameters (e.g. -d options) to be passed ***"
-echo "*** certbot --standalone $@"
+if [[ "$CERTHOST" == "" ]]; then
 
-certbot --standalone $@
+  echo "*** Running certbot in standalone mode, expecting additional parameters (e.g. -d options) to be passed ***"
+  echo "*** certbot --standalone --cert-path /opt/letsencrypt/certs $@"
+  certbot --standalone --cert-path /opt/letsencrypt/certs $@
+
+else
+
+  echo "*** Running certbot in standalone mode for host: $CERTHOST ***"
+  echo "*** certbot --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST"
+
+  certbot --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST
+
+fi
+
+echo "*** Creating secret"
+TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+
+oc login --token="$TOKEN" --insecure-skip-tls-verify https://kubernetes.default.svc.cluster.local
+
+EXISTS=$(oc get secret letsencrypt --no-headers | wc -l)
+[[ $EXISTS -gt 0 ]] && oc delete secret letsencrypt && echo "*** deleted existing secret"
+
+oc secret new letsencrypt /opt/letsencrypt/certs/
+oc describe secret letsencrypt
+
+
+
