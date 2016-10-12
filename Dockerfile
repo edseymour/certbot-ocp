0f8152a943b16a8a43a49c05248e96c93e9b51c8
edseymour <eseymour@redhat.com>
2016-10-12 14:09:59 +0100

    fixed oc commands, created /etc/letsencrypt dir

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,12 @@ USER root
 ADD https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm /epel-release-latest-7.noarch.rpm
 RUN yum install -y /epel-release-latest-7.noarch.rpm
 RUN yum install --enablerepo=rhel-7-server-optional-rpms --enablerepo=rhel-7-server-ose-3.3-rpms -y certbot atomic-openshift-clients && yum clean all
-RUN mkdir -p /opt/letsencrypt/certs
+RUN mkdir -p /opt/letsencrypt/certs && mkdir /etc/letsencrypt && chmod 666 /etc/letsencrypt
 WORKDIR /opt/letsencrypt
 ADD get-certs.sh /opt/letsencrypt/get-certs.sh 
-RUN chmod +x /opt/letsencrypt/get-certs.sh && chmod 777 /opt/letsencrypt/certs && chown 1001:1001 -R /opt/letsencrypt
+RUN chmod +x /opt/letsencrypt/get-certs.sh && chmod 777 /opt/letsencrypt/certs && chown 1001:1001 -R /opt/letsencrypt && chown 1001:1001 /etc/letsencrypt
 
 USER 1001
-VOLUME ['/opt/letsencrypt/certs']
+VOLUME ['/opt/letsencrypt/certs','/etc/letsencrypt']
 
 ENTRYPOINT ['/opt/letsencrypt/get-certs.sh']
--- get-certs.sh
+++ get-certs.sh
@@ -3,28 +3,27 @@
 if [[ "$CERTHOST" == "" ]]; then
 
   echo "*** Running certbot in standalone mode, expecting additional parameters (e.g. -d options) to be passed ***"
-  echo "*** certbot --standalone --cert-path /opt/letsencrypt/certs $@"
-  certbot --standalone --cert-path /opt/letsencrypt/certs $@
+  echo "*** certbot certonly --standalone --cert-path /opt/letsencrypt/certs $@"
+  certbot certonly --standalone --cert-path /opt/letsencrypt/certs $@
 
 else
 
   echo "*** Running certbot in standalone mode for host: $CERTHOST ***"
-  echo "*** certbot --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST"
+  echo "*** certbot certonly --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST"
 
-  certbot --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST
+  certbot certonly --standalone --cert-path /opt/letsencrypt/certs -d $CERTHOST
 
 fi
 
 echo "*** Creating secret"
-TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+OC_OPTS="--token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)  --insecure-skip-tls-verify https://kubernetes.default.svc.cluster.local"
 
-oc login --token="$TOKEN" --insecure-skip-tls-verify https://kubernetes.default.svc.cluster.local
 
-EXISTS=$(oc get secret letsencrypt --no-headers | wc -l)
-[[ $EXISTS -gt 0 ]] && oc delete secret letsencrypt && echo "*** deleted existing secret"
+EXISTS=$(oc get secret letsencrypt $OC_OPTS --no-headers | wc -l)
+[[ $EXISTS -gt 0 ]] && oc delete secret letsencrypt $OC_OPTS && echo "*** deleted existing secret"
 
-oc secret new letsencrypt /opt/letsencrypt/certs/
-oc describe secret letsencrypt
+oc secret new letsencrypt /opt/letsencrypt/certs/ $OC_OPTS
+oc describe secret letsencrypt $OC_OPTS
 
 
 
